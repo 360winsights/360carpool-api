2cb3bba31b3c384277f0315f31b1c6d1145eff5d
ankurkaushal <akaushal@360incentives.com>
2018-03-10 03:06:17 -0500

    Fix /drives/getRiders route

--- routes/drives.js
+++ routes/drives.js
@@ -6,14 +6,14 @@ const router = new Router()
 
 // get users riding with rider
 router.get('/drives/getRiders/:driver_id', (req, res) => {
-  conn.query(`
-    select drives.driver_id, users.driver_id
-    from drives
-    join users on drives.driver_id=users.driver_id
-    where drives.driver_id=${req.params.driver_id}
-  `), (error, results, fields) => {
-    res.end(JSON.stringify(results, null, 4))
-  }
-})
+	let query = 'select drives.driver_id, users.driver_id from drives join users on drives.driver_id=users.driver_id where drives.driver_id = ?'
+	conn.query(query, [req.params.driver_id], function (error, results, fields) {
+	 	if (error) {
+	      throw error
+	    }
+
+	    res.end(JSON.stringify(results, null, 4))
+	});
+});
 
 module.exports = router
