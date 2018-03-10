65330733484b115b02fde49bf03b0e958812da6a
David Negrazis <david.negrazis@outlook.com>
2018-03-09 19:21:18 -0500

    Minor cleanup

---
+++ main.js
@@ -0,0 +1,28 @@
+const config = require('./db-config/config.js')
+const restify = require('restify')
+const mysql = require('mysql')
+const conn = config.db.get
+
+// routes
+const userRouter = require('./routes/users.js')
+
+// server init
+const server = restify.createServer({
+    name: config.name,
+    version: config.version,
+    url: config.hostname
+})
+server.use(restify.plugins.acceptParser(server.acceptable))
+server.use(restify.plugins.queryParser())
+server.use(restify.plugins.bodyParser())
+
+// apply router routes
+userRouter.applyRoutes(server)
+
+server.listen(config.port, () => {
+  console.log('%s listening at %s', server.name, server.url)
+})
+
+server.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the Carpool API!'} )
+})
